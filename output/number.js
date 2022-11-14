a53fb546c5cb511bb55b9ff7f2c9281f0b9f7e30
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2022-11-14 00:52:53 -0300

    feat: study about number Type

---
+++ output/number.js
@@ -0,0 +1,7 @@
+"use strict";
+var ano;
+var ano2;
+ano = 2021;
+ano2 = 2048;
+console.log(ano + ano2);
+//# sourceMappingURL=number.js.map
---
+++ scripts/number.ts
@@ -0,0 +1,9 @@
+let ano: number;
+let ano2: number;
+
+ano = 2021;
+ano2 = 2048;
+
+console.log(ano + ano2);
+
+const numeroGigante: bigint = 50n;
